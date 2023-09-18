acc11b80adac1373dd81c7543fdb13d4337815dc
odansyyy <odansyaharta26@gmail.com>
2023-09-18 10:02:52 +0700

    initisl vommiy

---
+++ pertemuan3_1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php "saya sedang belajar php"; ?> </title>
+</head>
+<body>
+    <?php echo "Saya sedang belajar PHP"; ?>
+</body>
+</html>
---
+++ variabel.php
@@ -0,0 +1,17 @@
+<?php
+//    $nama_barang = "Minyak Goreng";
+//    $harga = 15000;
+//
+//    echo "ibu membeli $nama_barang seharga $harga";
+
+//define('NAMA_KONSTANTA','NILAI KONSTANTA');
+//const KONSTANTA="nilai konstanta";
+
+//echo "nilai konstanta : KONSTATANTA";
+//echo "NILAI KONSTANTA : NAMA_KONSTANTA";
+
+$jenis_kelamin='L'; //tipe data char
+$nama_lengkap="arie ariyanto"
+?>
+
+
